654377b3c7fe5bb9035d202029a1efeb5c790532
leocavalcante <lc@leocavalcante.com>
2023-12-13 22:30:30 -0300

    feat: PHPUnit and PHP-CS-Fixer

--- src/php.sh
+++ src/php.sh
@@ -6,10 +6,6 @@ repl() {
     run -- psysh
 }
 
-fix() {
-    run -- php-cs-fixer fix $@
-}
-
 at() {
     run "-p$1:$1" ${@:2}
 }
@@ -18,4 +14,16 @@ server() {
     port=${1:-80}
     dir=${2:-.}
     at "$port" php -S 0.0.0.0:"$port" -t "$dir"
+}
+
+init-phpunit() {
+    cp "$PHPCTL_DIR/phpunit.template.xml" phpunit.xml
+}
+
+phpunit() {
+    run -- vendor/bin/phpunit ${@}
+}
+
+php-cs-fixer() {
+    run -- vendor/bin/php-cs-fixer ${@}
 }
