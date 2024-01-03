22d499d76ccc71c8eed3e929c57958fbf622bc79
Juneba <jr290488@gmail.com>
2024-01-03 11:45:42 -0300

    feat: protecting the user git config file

--- src/docker.sh
+++ src/docker.sh
@@ -31,7 +31,7 @@ run() {
         --name "phpctl_$(openssl rand -hex 6)" \
         $(env | awk -F= '/^[[:alpha:]]/{print $1}' | sed 's/^/-e/') \
         -v /var/run/docker.sock:/var/run/docker.sock \
-        -v ~/.gitconfig:/root/.gitconfig \
+        -v ~/.gitconfig:/root/.gitconfig:ro \
         -v "$(pwd)":/usr/local/src -w /usr/local/src \
         --net host --entrypoint sh \
         ${args[@]} "$1" "$PHPCTL_IMAGE" -c "${*:2}"
