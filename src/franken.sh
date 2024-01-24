07e6ff679951c38f3edae812378f67ec2605a218
leocavalcante <leonardo.cavalcante@picpay.com>
2024-01-24 10:46:03 -0300

    refactor(frankenphp): Run using default runner

--- src/franken.sh
+++
@@ -1,5 +0,0 @@
-franken() {
-    $PHPCTL_RUNTIME run -v "$PWD:/app" \
-        -p 80:80 -p 443:443 -p 443:443/udp \
-        dunglas/frankenphp
-}
---
+++ src/frankenphp.sh
@@ -0,0 +1,3 @@
+frankenphp() {
+    PHPCTL_IMAGE=dunglas/frankenphp run -- frankenphp run --config /etc/caddy/Caddyfile --adapter caddyfile
+}
