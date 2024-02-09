29261894324ba4fffb41bc5aa77fda1da11451c2
Neuber Oliveira <neuber.oliveira05@gmail.com>
2024-02-09 11:35:20 -0300

    add some missing -e option for echo in installer for colorize (#19)

--- docs/phpctl-installer.sh
+++ docs/phpctl-installer.sh
@@ -9,7 +9,7 @@ else
     SYMLINK_DIR=$1
 fi
 
-echo "\033[0;33mInstalling phpctl at \033[0m$INSTALL_DIR"
+echo -e "\033[0;33mInstalling phpctl at \033[0m$INSTALL_DIR"
 if [ -d "$INSTALL_DIR" ]; then
     echo "The install directory is not empty. Attempting to remove it..."
     rm -rI $INSTALL_DIR
@@ -24,19 +24,20 @@ while kill -0 $PID 2> /dev/null; do
         sleep 0.1
     done
 done
+printf "\r"
 
-echo "\b "
+
 if [ -z "$1" ]; then
     echo -n "Sudo will be prompted to symlink the phpctl files."
 else
     echo -n "Files will be symlinked to ${SYMLINK_DIR}."
 fi
-echo -n " \033[0;32mDo you want to continue? (y/n)\033[0m "
+echo -e -n " \033[0;32mDo you want to continue? (y/n)\033[0m "
 read -r answer
 if [ "$answer" != "${answer#[Yy]}" ]; then
     $SUDO ${INSTALL_DIR}/scripts/symlink-bins.sh ${INSTALL_DIR}
 else
-    echo "\033[0;31mTo use phpctl globally, link the cloned script to your bin directory, like:\033[0m"
+    echo -e "\033[0;31mTo use phpctl globally, link the cloned script to your bin directory, like:\033[0m"
     echo ""
     for file in "${INSTALL_DIR}"/bin/*; do
         bin=$(basename "$file")
