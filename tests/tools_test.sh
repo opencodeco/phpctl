85c047895629e6f4c101586b06e4aa31312b8765
leocavalcante <lc@leocavalcante.com>
2024-03-03 21:02:05 -0300

    chore(tools): Bump

--- tests/tools_test.sh
+++ tests/tools_test.sh
@@ -1,43 +1,43 @@
 function test_box() {
-    assert_contains "Box version 4.5" "$(./bin/phpctl box --version)"
+    assert_matches "Box version 4\." "$(./bin/phpctl box --version)"
 }
 
 function test_couscous() {
-    assert_contains "Couscous 1.10" "$(./bin/phpctl couscous --version)"
+    assert_matches "Couscous 1\." "$(./bin/phpctl couscous --version)"
 }
 
 function test_exakat() {
     if [ -n "$WITH_EXAKAT" ]; then
-        assert_contains "Version : 2.6" "$(./bin/phpctl exakat version)"
+        assert_matches "Version : 2\." "$(./bin/phpctl exakat version)"
     fi
 }
 
 function test_infection() {
-    assert_contains "Infection - PHP Mutation Testing Framework version 0.27" "$(./bin/phpctl infection --version)"
+    assert_matches "Infection - PHP Mutation Testing Framework version 0\.27\." "$(./bin/phpctl infection --version)"
 }
 
 function test_php_cs_fixer() {
-    assert_contains "PHP CS Fixer 3.46" "$(./bin/phpctl php-cs-fixer --version)"
+    assert_matches "PHP CS Fixer 3\." "$(./bin/phpctl php-cs-fixer --version)"
 }
 
 function test_phpmd() {
-    assert_contains "PHPMD 2.15" "$(./bin/phpctl phpmd --version)"
+    assert_matches "PHPMD 2\." "$(./bin/phpctl phpmd --version)"
 }
 
 function test_phpstan() {
-    assert_contains "PHPStan - PHP Static Analysis Tool 1.10" "$(./bin/phpctl phpstan --version)"
+    assert_matches "PHPStan - PHP Static Analysis Tool 1\." "$(./bin/phpctl phpstan --version)"
 }
 
 function test_phpunit() {
-    assert_contains "PHPUnit 10.5" "$(./bin/phpctl phpunit --version)"
+    assert_matches "PHPUnit 11\." "$(./bin/phpctl phpunit --version)"
 }
 
 function test_pint() {
-    assert_contains "Pint 1.13" "$(./bin/phpctl pint --version)"
+    assert_matches "Pint 1\." "$(./bin/phpctl pint --version)"
 }
 
 function test_watchr() {
     if [ -z "$WITHOUT_WATCHR" ]; then
-        assert_contains "watchr command-line utility v0.4.1@b1b8dd5" "$(./bin/phpctl watchr --version)"
+        assert_matches "watchr command-line utility v0\.5\." "$(./bin/phpctl watchr --version)"
     fi
 }
